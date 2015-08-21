7af3741e29609be8197a274fc915c94504ebf89b
Ad Taylor <ad@adtaylor.co.uk>
2015-08-21 14:37:04 +0100

    players

--- middleware/root/index.js
+++ middleware/root/index.js
@@ -1,5 +1,5 @@
-
 'use strict';
+var restify = require('restify');
 
 /**
  * Routes
@@ -12,9 +12,16 @@ var routes = [];
  * Version: 1.0.0
  */
 
+
+// players/
+// players/keepers
+// players/defenders
+// players/midfielders
+// players/forwards
+
 routes.push({
   meta: {
-    name: 'getRoot',
+    name: 'getPlayers',
     method: 'GET',
     paths: [
       '/'
@@ -23,11 +30,49 @@ routes.push({
   },
   middleware: function(req, res, next) {
     res.send({
-      foo: 'bar'
+      foo: 'players'
     });
     return next();
   }
 });
+
+routes.push({
+  meta: {
+    name: 'getPlayersByType',
+    method: 'GET',
+    paths: [
+      '/players/:type'
+    ],
+    version: '1.0.0'
+  },
+  middleware: function(req, res, next) {
+    var filterWhitelist = ["keepers", "defenders", "midfielders", "forwards"];
+    if( filterWhitelist.indexOf(req.params.type) == -1 ) return next( new restify.NotFoundError('Invalid Filter') );
+    console.log(req.params);
+    res.send({
+      foo: 'players/' +req.params.type
+    });
+    return next();
+  }
+});
+
+routes.push({
+  meta: {
+    name: 'getPlayer',
+    method: 'GET',
+    paths: [
+      '/player/:id'
+    ],
+    version: '1.0.0'
+  },
+  middleware: function(req, res, next) {
+    res.send({
+      foo: 'player'
+    });
+    return next();
+  }
+});
+
 
 /**
  * Export
